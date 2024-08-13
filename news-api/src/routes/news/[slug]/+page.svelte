598cf045b92c179fc8e29e40cf350618c97cb7c5
eray-sen <eray.sen2@ogr.sakarya.edu.tr>
2024-08-13 11:08:42 +0200

    Update +page.svelte

--- news-api/src/routes/news/[slug]/+page.svelte
+++ news-api/src/routes/news/[slug]/+page.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
+	import { WhatsApp, Facebook, X } from 'svelte-share-buttons-component';
+
 	export let data; // Data from the server-side
 	const { matchedArticle } = data;
-	const { shareAPiKey } = data;
 	const { shortUrl } = data;
 
-	function handleShareButtonClick(service: string) {
-		const apiKey = shareAPiKey;
-		const link = matchedArticle.url;
-		const title = matchedArticle.title;
-		let shareUrl: string;
-
-		if (service === 'whatsapp') {
-			shareUrl = `https://www.shareaholic.com/api/share/?v=1&apitype=1&apikey=${apiKey}&service=${service}&title=${title}&link=${link}&templates[whatsapp][text]= ${title} - ${shortUrl}`;
-		} else {
-			shareUrl = `https://www.shareaholic.com/api/share/?v=1&apitype=1&apikey=${apiKey}&service=${service}&link=${link}&title=${title}`;
-		}
-
-		window.open(shareUrl, '_blank');
-	}
+	const url = matchedArticle.url;
+	const title = matchedArticle.title;
+	const short_url = shortUrl;
 </script>
 
 <article>
@@ -29,9 +19,9 @@
 
 <div class="buttons">
 	<h3>Share the news</h3>
-	<button id="facebook" on:click={() => handleShareButtonClick('facebook')}>Facebook</button>
-	<button id="x" on:click={() => handleShareButtonClick('x')}>Twitter</button>
-	<button id="whatsapp" on:click={() => handleShareButtonClick('whatsapp')}>Whatsapp</button>
+	<Facebook class="share-button" quote={title} {url} />
+	<WhatsApp class="share-button" text="{title} {short_url}" />
+	<X class="share-button" text={title} {url} />
 </div>
 
 <style>
@@ -62,32 +52,10 @@
 	a:hover {
 		color: gray;
 	}
-	button {
-		margin-top: 20px;
-		padding: 10px 20px;
-		background-color: #4267b2;
-		color: white;
-		border: none;
-		border-radius: 5px;
-		cursor: pointer;
-		border-radius: 20px;
-	}
-	button:hover {
-		background-color: #365899;
-	}
+
 	.buttons {
 		margin-left: 70%;
 		text-align: center;
 		margin-bottom: 20px;
-	}
-	#x {
-		background-color: #1da1f2;
-		color: black;
-	}
-	#facebook {
-		background-color: #1877f2;
-	}
-	#whatsapp {
-		background-color: #25d366;
 	}
 </style>
